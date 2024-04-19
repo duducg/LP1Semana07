4f0f699a24b7f16070a5267bdc9b50267317c599
duducg <edu.sang.rocha@gmail.com>
2024-04-19 15:53:46 +0100

    Changed Unit Class

--- GameUnits/Unit.cs
+++ GameUnits/Unit.cs
@@ -1,14 +1,22 @@
+using System;
+
 namespace GameUnits
 {
-    public class Unit
+    //class is now abstract because it has at least 1 abstract member
+    public abstract class Unit 
     {
         private int movement;
-        public int Health { get; set; }
+        public virtual int Health { get; set; } //virtual can be changed
+        public abstract float Cost { get; } //cost cant be changed in derived
 
         public Unit(int movement, int health)
         {
             this.movement = movement;
             Health = health;
+        }
+        public void Move()
+        {
+            Console.WriteLine(movement);
         }
     }
 }
