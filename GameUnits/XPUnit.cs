cf8bf7a80ca3e6bffceb453dd2a6fa37e0e7ab18
duducg <edu.sang.rocha@gmail.com>
2024-04-19 17:30:18 +0100

    Created XPUnit class

---
+++ GameUnits/XPUnit.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameUnits
+{
+    public abstract class XPUnit:Unit
+    {
+        public int XP { get; protected set; }
+
+         public override float Cost { 
+            get
+            {
+                return 5f;
+            } 
+        }
+        public XPUnit(int mov, int health):base(mov, health)
+        {
+            XP = 0;
+        }
+        public override string ToString()
+        {
+            return base.ToString() + $" XP={XP}";
+        }
+
+    }
+}
